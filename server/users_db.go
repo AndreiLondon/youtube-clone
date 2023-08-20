c925786fe45ec9724bb2da67dacfa9eaa4f1b4d8
AndrewLondon <andrewmart@mail.com>
2023-08-20 23:20:52 +0100

    UserDb Updated

--- server/users_db.go
+++ server/users_db.go
@@ -1,5 +1,22 @@
 package main
 
-func createUsersTable() {
+import (
+	"database/sql"
 
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+
+// _________users_________________________________________________
+// |  id      |  email    |  username  |  password  |  sessionId  |
+// |  INTEGER |  TEXT     |  TEXT      |  TEXT      |  TEXT       |
+func crerateUsersTable() error {
+	statement, err := db.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, email TEXT NOT NULL UNIQUE, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, sessionId TEXT)")
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+	statement.Exec()
+	return nil
 }
